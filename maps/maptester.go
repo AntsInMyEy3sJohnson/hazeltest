e2b4af937c429da912a18485467167622e382c8c
Nico Krieg <n.krieg123@googlemail.com>
2022-04-11 18:57:52 +0200

    Refactor Pokedex-related functionality into corresponding Go file.

--- maps/maptester.go
+++ maps/maptester.go
@@ -5,38 +5,10 @@ type MapTester struct {
 	HzMemberAddresses	[]string
 }
 
-type pokedex struct {
-	Pokemon	[]pokemon	`json:"pokemon"`
-}
-
-type pokemon struct {
-	ID				int					`json:"id"`
-	Num				string				`json:"num"`
-	Name			string				`json:"name"`
-	Img				string				`json:"img"`
-	ElementType		[]string			`json:"type"`
-	Height			string				`json:"height"`
-	Weight			string				`json:"weight"`
-	Candy			string				`json:"candy"`
-	CandyCount		int					`json:"candy_count"`
-	EggDistance		string				`json:"egg"`
-	SpawnChance		float32				`json:"spawn_chance"`
-	AvgSpawns		float32					`json:"avg_spawns"`
-	SpawnTime		string				`json:"spawn_time"`
-	Multipliers		[]float32			`json:"multipliers"`
-	Weaknesses		[]string			`json:"weaknesses"`
-	NextEvolution	[]nextEvolution		`json:"next_evolution"`
-}
-
-type nextEvolution struct {
-	Num		string		`json:"num"`
-	Name	string		`json:"name"`
-}
-
 func (tester *MapTester) TestMaps() {
 
 	for _, runner := range MapRunners {
-		runner.Run("hazeltest-maptester", []string{"10.211.55.6"})
+		runner.Run(tester.HzCluster, tester.HzMemberAddresses)
 	}
 
 }
--- maps/pokedexrunner.go
+++ maps/pokedexrunner.go
@@ -4,13 +4,47 @@ import (
 	"context"
 	"embed"
 	"encoding/json"
+	"encoding/gob"
+	"errors"
+	"fmt"
 	"hazeltest/client"
+
+	"github.com/hazelcast/hazelcast-go-client"
 )
 
-type PokedexRunner struct {}
+type PokedexRunner struct{}
+
+type pokedex struct {
+	Pokemon []pokemon `json:"pokemon"`
+}
+
+type pokemon struct {
+	ID            int             `json:"id"`
+	Num           string          `json:"num"`
+	Name          string          `json:"name"`
+	Img           string          `json:"img"`
+	ElementType   []string        `json:"type"`
+	Height        string          `json:"height"`
+	Weight        string          `json:"weight"`
+	Candy         string          `json:"candy"`
+	CandyCount    int             `json:"candy_count"`
+	EggDistance   string          `json:"egg"`
+	SpawnChance   float32         `json:"spawn_chance"`
+	AvgSpawns     float32         `json:"avg_spawns"`
+	SpawnTime     string          `json:"spawn_time"`
+	Multipliers   []float32       `json:"multipliers"`
+	Weaknesses    []string        `json:"weaknesses"`
+	NextEvolution []nextEvolution `json:"next_evolution"`
+}
+
+type nextEvolution struct {
+	Num  string `json:"num"`
+	Name string `json:"name"`
+}
 
 func init() {
 	Register(PokedexRunner{})
+	gob.Register(pokemon{})
 }
 
 //go:embed pokedex.json
@@ -18,16 +52,8 @@ var pokedexFile embed.FS
 
 func (r PokedexRunner) Run(hzCluster string, hzMembers []string) {
 
-	pokedexJson, err := pokedexFile.Open("pokedex.json")
+	pokedex, err := parsePokedexFile()
 
-	if err != nil {
-		panic(err)
-	}
-	defer pokedexJson.Close()
-
-	var pokedex *pokedex
-	err = json.NewDecoder(pokedexJson).Decode(&pokedex)
-	
 	if err != nil {
 		panic(err)
 	}
@@ -41,20 +67,77 @@ func (r PokedexRunner) Run(hzCluster string, hzMembers []string) {
 	}
 	defer hzClient.Shutdown(ctx)
 
-	pokedexItems, err := hzClient.GetMap(ctx, "pokedex")
+	hzPokedexMap, err := hzClient.GetMap(ctx, "pokedex")
 
 	if err != nil {
 		panic(err)
 	}
 
-	for _, pokemon := range pokedex.Pokemon {
-		containsKey, err := pokedexItems.ContainsKey(ctx, pokemon.ID)
+	err = ingestAll(ctx, hzPokedexMap, pokedex)
+	if err != nil {
+		panic(err)
+	}
+
+	err = readAll(ctx, hzPokedexMap, pokedex)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func readAll(ctx context.Context, m *hazelcast.Map, p *pokedex) error {
+
+	for _, v := range p.Pokemon {
+		valueFromHZ, err := m.Get(ctx, v.ID)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		if !containsKey {
-			pokedexItems.Set(ctx, pokemon.ID, pokemon)
+		_, ok := valueFromHZ.(pokemon)
+		if !ok {
+			return errors.New("unable to serialize value retrieved from hazelcast map into pokemon instance")
 		}
 	}
+
+	fmt.Printf("retrieved %d items from hazelcast map\n", len(p.Pokemon))
+
+	return nil
+
+}
+
+func ingestAll(ctx context.Context, m *hazelcast.Map, p *pokedex) error {
+
+	for _, v := range p.Pokemon {
+		containsKey, err := m.ContainsKey(ctx, v.ID)
+		if err != nil {
+			return err
+		}
+		if !containsKey {
+			m.Set(ctx, v.ID, v)
+		}
+	}
+
+	fmt.Printf("stored %d items in hazelcast map\n", len(p.Pokemon))
+
+	return nil
+
+}
+
+func parsePokedexFile() (*pokedex, error) {
+
+	pokedexJson, err := pokedexFile.Open("pokedex.json")
+
+	if err != nil {
+		return nil, err
+	}
+	defer pokedexJson.Close()
+
+	var pokedex pokedex
+	err = json.NewDecoder(pokedexJson).Decode(&pokedex)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pokedex, nil
 
 }
