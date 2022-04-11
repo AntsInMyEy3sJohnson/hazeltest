8d31918b4d168191b1cd2ef222f78aee8efc37dd
Nico Krieg <n.krieg123@googlemail.com>
2022-04-11 15:42:58 +0200

    Add simple Hazelcast interactions

--- hazeltest.go
+++ hazeltest.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/hazelcast/hazelcast-go-client"
 )
 
 func main() {
 
-	fmt.Println("Hello, world!")
+	ctx := context.TODO()
+
+	hzConfig := &hazelcast.Config{}
+	hzConfig.ClientName = "hazeltest"
+	hzConfig.Cluster.Name = "hazelcastimdg"
+	hzConfig.Cluster.Network.SetAddresses("10.211.55.6")
+
+	hzClient, err := hazelcast.StartNewClientWithConfig(ctx, *hzConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	strings, err := hzClient.GetMap(ctx, "strings")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := strings.Set(ctx, "mykey", "myvalue"); err != nil {
+		panic(err)
+	}
+
+	if value, err := strings.Get(ctx, "mykey"); err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Retrieved value from map: %s\n", value)
+	}
+
+	hzClient.Shutdown(ctx)
 
 }
