63f04dae368697967063959565162e2714eef139
Nico Krieg <n.krieg123@googlemail.com>
2022-07-03 18:20:28 +0200

    Add sleep configs for put and poll operations of tweet runner.

--- queues/runnerconfig.go
+++ queues/runnerconfig.go
@@ -17,9 +17,17 @@ type RunnerConfig struct {
 }
 
 type OperationConfig struct {
-	Enabled   bool
-	NumRuns   int
-	BatchSize int
+	Enabled                   bool
+	NumRuns                   int
+	BatchSize                 int
+	InitialDelay              *SleepConfig
+	SleepBetweenActionBatches *SleepConfig
+	SleepBetweenRuns          *SleepConfig
+}
+
+type SleepConfig struct {
+	Enabled    bool
+	DurationMs int
 }
 
 type RunnerConfigBuilder struct {
@@ -28,15 +36,36 @@ type RunnerConfigBuilder struct {
 	ParsedConfig  map[string]interface{}
 }
 
+// constants related to general runner configuration
 const (
-	defaultEnabled       = true
-	defaultNumQueues     = 10
-	defaultPutEnabled    = true
-	defaultNumRunsPut    = 5000
-	defaultBatchSizePut  = 50
-	defaultPollEnabled   = true
-	defaultNumRunsPoll   = 10000
-	defaultBatchSizePoll = 50
+	defaultEnabled   = true
+	defaultNumQueues = 10
+)
+
+// constants related to put configuration
+const (
+	defaultEnabledPut                             = true
+	defaultNumRunsPut                             = 2000
+	defaultBatchSizePut                           = 50
+	defaultSleepInitialDelayEnabledPut            = true
+	defaultSleepInitialDelayDurationMsPut         = 100
+	defaultSleepBetweenActionBatchesEnabledPut    = true
+	defaultSleepBetweenActionBatchesDurationMsPut = 100
+	defaultSleepBetweenRunsEnabledPut             = true
+	defaultSleepBetweenRunsDurationMsPut          = 500
+)
+
+// constants related to poll configuration
+const (
+	defaultEnabledPoll                             = true
+	defaultNumRunsPoll                             = 2000
+	defaultBatchSizePoll                           = 50
+	defaultSleepInitialDelayEnabledPoll            = true
+	defaultSleepInitialDelayDurationMsPoll         = 2000
+	defaultSleepBetweenActionBatchesEnabledPoll    = true
+	defaultSleepBetweenActionBatchesDurationMsPoll = 200
+	defaultSleepBetweenRunsEnabledPoll             = true
+	defaultSleepBetweenRunsDurationMsPoll          = 1000
 )
 
 func (b RunnerConfigBuilder) PopulateConfig() *RunnerConfig {
@@ -61,73 +90,107 @@ func (b RunnerConfigBuilder) PopulateConfig() *RunnerConfig {
 		numQueues = valueFromConfig.(int)
 	}
 
-	keyPath = b.RunnerKeyPath + ".putConfig.enabled"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var putEnabled bool
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		putEnabled = defaultPutEnabled
-	} else {
-		putEnabled = valueFromConfig.(bool)
-	}
-
-	keyPath = b.RunnerKeyPath + ".putConfig.numRuns"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var numRunsPut int
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		numRunsPut = defaultNumRunsPut
-	} else {
-		numRunsPut = valueFromConfig.(int)
-	}
-
-	keyPath = b.RunnerKeyPath + ".putConfig.batchSize"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var batchSizePut int
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		batchSizePut = defaultBatchSizePut
-	} else {
-		batchSizePut = valueFromConfig.(int)
-	}
-
-	keyPath = b.RunnerKeyPath + ".pollConfig.enabled"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var pollEnabled bool
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		pollEnabled = defaultPollEnabled
-	} else {
-		pollEnabled = valueFromConfig.(bool)
-	}
-
-	keyPath = b.RunnerKeyPath + ".pollConfig.numRuns"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var numRunsPoll int
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		numRunsPoll = defaultNumRunsPoll
-	} else {
-		numRunsPoll = valueFromConfig.(int)
-	}
-
-	keyPath = b.RunnerKeyPath + ".pollConfig.batchSize"
-	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
-	var batchSizePoll int
-	if err != nil {
-		logErrUponConfigExtraction(keyPath, err)
-		batchSizePoll = defaultBatchSizePoll
-	} else {
-		batchSizePoll = valueFromConfig.(int)
-	}
-
 	return &RunnerConfig{
 		Enabled:       enabled,
 		NumQueues:     numQueues,
 		QueueBaseName: b.QueueBaseName,
-		PutConfig:     &OperationConfig{putEnabled, numRunsPut, batchSizePut},
-		PollConfig:    &OperationConfig{pollEnabled, numRunsPoll, batchSizePoll},
+		PutConfig:     b.populateOperationConfig("put", defaultEnabledPut, defaultNumRunsPut, defaultBatchSizePut),
+		PollConfig:    b.populateOperationConfig("poll", defaultEnabledPoll, defaultNumRunsPoll, defaultBatchSizePoll),
 	}
+
+}
+
+func (b RunnerConfigBuilder) populateOperationConfig(operation string, defaultOperationEnabled bool,
+	defaultNumRuns int, defaultBatchSize int) *OperationConfig {
+
+	operationConfig := b.RunnerKeyPath + "." + fmt.Sprintf("%sConfig", operation)
+	keyPath := operationConfig + ".enabled"
+	valueFromConfig, err := config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var enabled bool
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		enabled = defaultOperationEnabled
+	} else {
+		enabled = valueFromConfig.(bool)
+	}
+
+	keyPath = operationConfig + ".numRuns"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var numRuns int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		numRuns = defaultNumRuns
+	} else {
+		numRuns = valueFromConfig.(int)
+	}
+
+	keyPath = operationConfig + ".batchSize"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var batchSizePoll int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		batchSizePoll = defaultBatchSize
+	} else {
+		batchSizePoll = valueFromConfig.(int)
+	}
+
+	var defaultInitialDelayEnabled bool
+	var defaultInitialDelayDurationMs int
+	var defaultBetweenActionBatchesEnabled bool
+	var defaultBetweenActionBatchesDurationMs int
+	var defaultBetweenRunsEnabled bool
+	var defaultBetweenRunsDurationMs int
+
+	if operation == "put" {
+		defaultInitialDelayEnabled = defaultSleepInitialDelayEnabledPut
+		defaultInitialDelayDurationMs = defaultSleepInitialDelayDurationMsPut
+		defaultBetweenActionBatchesEnabled = defaultSleepBetweenActionBatchesEnabledPut
+		defaultBetweenActionBatchesDurationMs = defaultSleepBetweenActionBatchesDurationMsPut
+		defaultBetweenRunsEnabled = defaultSleepBetweenRunsEnabledPut
+		defaultBetweenRunsDurationMs = defaultSleepBetweenRunsDurationMsPut
+	} else {
+		defaultInitialDelayEnabled = defaultSleepInitialDelayEnabledPoll
+		defaultInitialDelayDurationMs = defaultSleepInitialDelayDurationMsPoll
+		defaultBetweenActionBatchesEnabled = defaultSleepBetweenActionBatchesEnabledPoll
+		defaultBetweenActionBatchesDurationMs = defaultSleepBetweenActionBatchesDurationMsPoll
+		defaultBetweenRunsEnabled = defaultSleepBetweenRunsEnabledPoll
+		defaultBetweenRunsDurationMs = defaultSleepBetweenRunsDurationMsPoll
+	}
+
+	return &OperationConfig{
+		Enabled:                   enabled,
+		NumRuns:                   numRuns,
+		BatchSize:                 batchSizePoll,
+		InitialDelay:              b.populateSleepConfig(operationConfig+".sleeps.initialDelay", defaultInitialDelayEnabled, defaultInitialDelayDurationMs),
+		SleepBetweenActionBatches: b.populateSleepConfig(operationConfig+".sleeps.betweenActionBatches", defaultBetweenActionBatchesEnabled, defaultBetweenActionBatchesDurationMs),
+		SleepBetweenRuns:          b.populateSleepConfig(operationConfig+".sleeps.betweenRuns", defaultBetweenRunsEnabled, defaultBetweenRunsDurationMs),
+	}
+
+}
+
+func (b RunnerConfigBuilder) populateSleepConfig(configBasePath string, defaultEnabled bool, defaultDurationMs int) *SleepConfig {
+
+	keyPath := configBasePath + ".enabled"
+	valueFromConfig, err := config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var enabled bool
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		enabled = defaultEnabled
+	} else {
+		enabled = valueFromConfig.(bool)
+	}
+
+	keyPath = configBasePath + ".durationMs"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var durationMs int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		durationMs = defaultDurationMs
+	} else {
+		durationMs = valueFromConfig.(int)
+	}
+
+	return &SleepConfig{enabled, durationMs}
 
 }
 
