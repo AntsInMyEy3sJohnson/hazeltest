0b7455233e58495baefc9fa13d19d22261a87adc
Nico Krieg <n.krieg123@googlemail.com>
2022-07-03 14:26:26 +0200

    Make put and poll behavior configurable individually.

--- queues/runnerconfig.go
+++ queues/runnerconfig.go
@@ -9,13 +9,35 @@ import (
 )
 
 type RunnerConfig struct {
-	Enabled bool
+	Enabled       bool
+	NumQueues     int
+	QueueBaseName string
+	PutConfig     *OperationConfig
+	PollConfig    *OperationConfig
+}
+
+type OperationConfig struct {
+	Enabled   bool
+	NumRuns   int
+	BatchSize int
 }
 
 type RunnerConfigBuilder struct {
 	RunnerKeyPath string
+	QueueBaseName string
 	ParsedConfig  map[string]interface{}
 }
+
+const (
+	defaultEnabled       = true
+	defaultNumQueues     = 10
+	defaultPutEnabled    = true
+	defaultNumRunsPut    = 5000
+	defaultBatchSizePut  = 50
+	defaultPollEnabled   = true
+	defaultNumRunsPoll   = 10000
+	defaultBatchSizePoll = 50
+)
 
 func (b RunnerConfigBuilder) PopulateConfig() *RunnerConfig {
 
@@ -29,13 +51,85 @@ func (b RunnerConfigBuilder) PopulateConfig() *RunnerConfig {
 		enabled = valueFromConfig.(bool)
 	}
 
-	return &RunnerConfig{Enabled: enabled}
+	keyPath = b.RunnerKeyPath + ".numQueues"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var numQueues int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		numQueues = defaultNumQueues
+	} else {
+		numQueues = valueFromConfig.(int)
+	}
+
+	keyPath = b.RunnerKeyPath + ".putConfig.enabled"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var putEnabled bool
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		putEnabled = defaultPutEnabled
+	} else {
+		putEnabled = valueFromConfig.(bool)
+	}
+
+	keyPath = b.RunnerKeyPath + ".putConfig.numRuns"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var numRunsPut int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		numRunsPut = defaultNumRunsPut
+	} else {
+		numRunsPut = valueFromConfig.(int)
+	}
+
+	keyPath = b.RunnerKeyPath + ".putConfig.batchSize"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var batchSizePut int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		batchSizePut = defaultBatchSizePut
+	} else {
+		batchSizePut = valueFromConfig.(int)
+	}
+
+	keyPath = b.RunnerKeyPath + ".pollConfig.enabled"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var pollEnabled bool
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		pollEnabled = defaultPollEnabled
+	} else {
+		pollEnabled = valueFromConfig.(bool)
+	}
+
+	keyPath = b.RunnerKeyPath + ".pollConfig.numRuns"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var numRunsPoll int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		numRunsPoll = defaultNumRunsPoll
+	} else {
+		numRunsPoll = valueFromConfig.(int)
+	}
+
+	keyPath = b.RunnerKeyPath + ".pollConfig.batchSize"
+	valueFromConfig, err = config.ExtractConfigValue(b.ParsedConfig, keyPath)
+	var batchSizePoll int
+	if err != nil {
+		logErrUponConfigExtraction(keyPath, err)
+		batchSizePoll = defaultBatchSizePoll
+	} else {
+		batchSizePoll = valueFromConfig.(int)
+	}
+
+	return &RunnerConfig{
+		Enabled:       enabled,
+		NumQueues:     numQueues,
+		QueueBaseName: b.QueueBaseName,
+		PutConfig:     &OperationConfig{putEnabled, numRunsPut, batchSizePut},
+		PollConfig:    &OperationConfig{pollEnabled, numRunsPoll, batchSizePoll},
+	}
 
 }
-
-const (
-	defaultEnabled = true
-)
 
 func logErrUponConfigExtraction(keyPath string, err error) {
 
