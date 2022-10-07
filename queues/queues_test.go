9bba83e35e56739d061fb626473eb453a30dd66c
Nico Krieg <n.krieg123@googlemail.com>
2022-10-07 11:25:22 +0300

    Implement dummy hz queue store.

--- queues/queues_test.go
+++ queues/queues_test.go
@@ -1,10 +1,18 @@
 package queues
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/hazelcast/hazelcast-go-client"
+)
+
 type (
 	testConfigPropertyAssigner struct {
 		returnError bool
 		dummyConfig map[string]interface{}
 	}
+	dummyHzQueueStore struct{}
 )
 
 const (
@@ -14,3 +22,49 @@ const (
 	queuePrefix   = "t_"
 	queueBaseName = "test"
 )
+
+var (
+	hzCluster = "awesome-hz-cluster"
+	hzMembers = []string{"awesome-hz-cluster-svc.cluster.local"}
+)
+
+func (d dummyHzQueueStore) Shutdown(_ context.Context) error {
+	return nil
+}
+
+func (d dummyHzQueueStore) InitHazelcastClient(_ context.Context, _ string, _ string, _ []string) {
+	// No-op
+}
+
+func (d dummyHzQueueStore) GetQueue(_ context.Context, _ string) (*hazelcast.Queue, error) {
+	return nil, errors.New("it is but a scratch")
+}
+
+func (a testConfigPropertyAssigner) Assign(keyPath string, assignFunc func(any)) error {
+
+	if a.returnError {
+		return errors.New("lo and behold, here is a deliberately thrown error")
+	}
+
+	if value, ok := a.dummyConfig[keyPath]; ok {
+		assignFunc(value)
+	}
+
+	return nil
+}
+
+func checkRunnerStateTransitions(expected []state, actual []state) (string, bool) {
+
+	if len(expected) != len(actual) {
+		return fmt.Sprintf("expected %d state transition(-s), got %d", len(expected), len(actual)), false
+	}
+
+	for i, expectedValue := range expected {
+		if actual[i] != expectedValue {
+			return fmt.Sprintf("expected '%s' in index '%d', got '%s'", expectedValue, i, actual[i]), false
+		}
+	}
+
+	return "", true
+
+}
