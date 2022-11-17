8f6a1284c429ddf267d1e66ef112a029a7f52e74
Nico Krieg <n.krieg123@googlemail.com>
2022-11-17 20:09:59 +0100

    Introduce test cases for readiness handler.

--- api/endpoints_test.go
+++ api/endpoints_test.go
@@ -11,6 +11,127 @@ import (
 	"testing"
 )
 
+func TestStatusHandler(t *testing.T) {
+
+	t.Log("given the need to test the status handler, serving the application's status endpoint")
+	{
+		t.Log("\twhen http method other than get is passed")
+		{
+			recorder := httptest.NewRecorder()
+
+			livenessHandler(recorder, httptest.NewRequest(http.MethodPost, "localhost:8080/status", nil))
+			response := recorder.Result()
+			defer response.Body.Close()
+
+			expectedStatusCode := http.StatusMethodNotAllowed
+			msg := fmt.Sprintf("\t\tstatus handler must return http status %d", expectedStatusCode)
+			checkStatusCode(t, expectedStatusCode, response.StatusCode, msg)
+		}
+
+		t.Log("\twhen no test loops have registered yet")
+		{
+			request := httptest.NewRequest(http.MethodGet, "localhost:8080/status", nil)
+			recorder := httptest.NewRecorder()
+
+			statusHandler(recorder, request)
+			response := recorder.Result()
+			defer response.Body.Close()
+
+			expectedStatusCode := http.StatusOK
+			msg := fmt.Sprintf("\t\tstatus handler must return http status %d", expectedStatusCode)
+			checkStatusCode(t, expectedStatusCode, response.StatusCode, msg)
+
+			data, err := tryResponseRead(response.Body)
+			msg = "\t\tresponse must be readable"
+			if err == nil {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tresponse body must be valid json"
+			var decodedData map[string]interface{}
+			err = json.Unmarshal(data, &decodedData)
+			if err == nil {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tdecoded artifact must be empty map"
+			if len(decodedData) == 0 {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+		}
+
+		t.Log("\twhen two test loops have registered")
+		{
+			RegisterTestLoop(sourcePokedexRunner, func() map[string]interface{} {
+				return dummyStatusPokedexTestLoop
+			})
+			RegisterTestLoop(sourceLoadRunner, func() map[string]interface{} {
+				return dummyStatusLoadTestLoop
+			})
+
+			request := httptest.NewRequest(http.MethodGet, "localhost:8080/status", nil)
+			recorder := httptest.NewRecorder()
+
+			statusHandler(recorder, request)
+			response := recorder.Result()
+			defer response.Body.Close()
+
+			expectedStatusCode := http.StatusOK
+			msg := fmt.Sprintf("\t\tstatus handler must return http status %d", expectedStatusCode)
+			checkStatusCode(t, expectedStatusCode, response.StatusCode, msg)
+
+			data, _ := tryResponseRead(response.Body)
+
+			msg = "\t\tresponse body must be valid json"
+			var decodedData map[string]interface{}
+			err := json.Unmarshal(data, &decodedData)
+			if err == nil {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tdecoded map must contain top-level keys for both registered test loops"
+			if _, okSourcePokedex := decodedData[sourcePokedexRunner]; okSourcePokedex {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, sourcePokedexRunner)
+			}
+			if _, okSourceLoad := decodedData[sourceLoadRunner]; okSourceLoad {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, sourceLoadRunner)
+			}
+
+			msg = "\t\tnested maps must be equal to registered status"
+			statusPokedexTestLoop := decodedData[sourcePokedexRunner].(map[string]interface{})
+			parseNumberValuesBackToInt(statusPokedexTestLoop)
+			if ok, detail := mapsEqualInContent(dummyStatusPokedexTestLoop, statusPokedexTestLoop); ok {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, detail)
+			}
+
+			statusLoadTestLoop := decodedData[sourceLoadRunner].(map[string]interface{})
+			parseNumberValuesBackToInt(statusLoadTestLoop)
+			if ok, detail := mapsEqualInContent(dummyStatusLoadTestLoop, statusLoadTestLoop); ok {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, detail)
+			}
+
+		}
+	}
+
+}
+
 func TestLivenessHandler(t *testing.T) {
 
 	t.Log("given the need to test the liveness handler, serving the application's liveness check")
@@ -162,6 +283,14 @@ func TestReadinessHandler(t *testing.T) {
 		}
 
 	}
+
+}
+
+func parseNumberValuesBackToInt(m map[string]interface{}) {
+
+	m[statusKeyNumMaps] = int(m[statusKeyNumMaps].(float64))
+	m[statusKeyNumRuns] = int(m[statusKeyNumRuns].(float64))
+	m[statusKeyTotalRuns] = int(m[statusKeyTotalRuns].(float64))
 
 }
 
