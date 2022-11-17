5be97edf6133f5a92d1d2762f09de3b5c6bc3390
Nico Krieg <n.krieg123@googlemail.com>
2022-11-17 20:09:36 +0100

    Move commonly used identifiers to common file.

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,52 @@
 package api
 
+import "fmt"
+
+const (
+	sourcePokedexRunner = "pokedexrunner"
+	sourceLoadRunner    = "loadrunner"
+)
+const (
+	statusKeyNumMaps        = "numMaps"
+	statusKeyNumRuns        = "numRuns"
+	statusKeyTotalRuns      = "totalRuns"
+	statusKeyRunnerFinished = "runnerFinished"
+)
+
 const (
 	checkMark = "\u2713"
 	ballotX   = "\u2717"
 )
+
+var (
+	dummyStatusPokedexTestLoop = map[string]interface{}{
+		statusKeyNumMaps:        10,
+		statusKeyNumRuns:        1000,
+		statusKeyTotalRuns:      10 * 1000,
+		statusKeyRunnerFinished: false,
+	}
+	dummyStatusLoadTestLoop = map[string]interface{}{
+		statusKeyNumMaps:        5,
+		statusKeyNumRuns:        100,
+		statusKeyTotalRuns:      5 * 100,
+		statusKeyRunnerFinished: false,
+	}
+)
+
+func mapsEqualInContent(reference map[string]interface{}, candidate map[string]interface{}) (bool, string) {
+
+	if len(reference) != len(candidate) {
+		return false, "given maps do not have same length, hence cannot have equal content"
+	}
+
+	for k1, v1 := range reference {
+		if v2, ok := candidate[k1]; !ok {
+			return false, fmt.Sprintf("key wanted in candidate map, but not found: %s", k1)
+		} else if v1 != v2 {
+			return false, fmt.Sprintf("key '%s' associated with different values -- wanted: %v; got: %v", k1, v1, v2)
+		}
+	}
+
+	return true, ""
+
+}
--- api/status_test.go
+++ api/status_test.go
@@ -1,36 +1,8 @@
 package api
 
 import (
-	"fmt"
 	"sync"
 	"testing"
-)
-
-const (
-	statusKeyNumMaps        = "numMaps"
-	statusKeyNumRuns        = "numRuns"
-	statusKeyTotalRuns      = "totalRuns"
-	statusKeyRunnerFinished = "runnerFinished"
-)
-
-const (
-	sourcePokedexRunner = "pokedexrunner"
-	sourceLoadRunner    = "loadrunner"
-)
-
-var (
-	dummyStatusPokedexTestLoop = map[string]interface{}{
-		statusKeyNumMaps:        10,
-		statusKeyNumRuns:        1000,
-		statusKeyTotalRuns:      10 * 1000,
-		statusKeyRunnerFinished: false,
-	}
-	dummyStatusLoadTestLoop = map[string]interface{}{
-		statusKeyNumMaps:        5,
-		statusKeyNumRuns:        100,
-		statusKeyTotalRuns:      5 * 100,
-		statusKeyRunnerFinished: false,
-	}
 )
 
 func TestAssembleTestLoopStatus(t *testing.T) {
@@ -140,23 +112,5 @@ func checkAssociatedStatusIsEmptyMap(t *testing.T, topLevelElementKey string, as
 	} else {
 		t.Error(msg, ballotX)
 	}
-
-}
-
-func mapsEqualInContent(reference map[string]interface{}, candidate map[string]interface{}) (bool, string) {
-
-	if len(reference) != len(candidate) {
-		return false, "given maps do not have same length, hence cannot have equal content"
-	}
-
-	for k1, v1 := range reference {
-		if v2, ok := candidate[k1]; !ok {
-			return false, fmt.Sprintf("key wanted in candidate map, but not found: %s", k1)
-		} else if v1 != v2 {
-			return false, fmt.Sprintf("key '%s' associated with different values -- wanted: %v; got: %v", k1, v1, v2)
-		}
-	}
-
-	return true, ""
 
 }
