f348c2330c7a1cbefb19bed10d2755538ebead96
Nico Krieg <n.krieg123@googlemail.com>
2022-11-22 20:52:12 +0100

    Add test case for listen method.

--- status/gatherer_test.go
+++ status/gatherer_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"testing"
 )
 
@@ -12,12 +13,30 @@ type (
 		m                    sync.Mutex
 		numLocks, numUnlocks int
 	}
+	stateExposingWaitGroup struct {
+		sync.WaitGroup
+		count int32
+	}
 )
 
 const (
 	checkMark = "\u2713"
 	ballotX   = "\u2717"
 )
+
+func (wg *stateExposingWaitGroup) add(delta int) {
+	atomic.AddInt32(&wg.count, int32(delta))
+	wg.WaitGroup.Add(delta)
+}
+
+func (wg *stateExposingWaitGroup) done() {
+	atomic.AddInt32(&wg.count, -1)
+	wg.WaitGroup.Done()
+}
+
+func (wg *stateExposingWaitGroup) waitingCount() int {
+	return int(atomic.LoadInt32(&wg.count))
+}
 
 func (l *testLocker) lock() {
 
@@ -33,34 +52,12 @@ func (l *testLocker) unlock() {
 
 }
 
-func TestGatherer_InsertSynchronously(t *testing.T) {
+func TestGatherer_Listen(t *testing.T) {
 
-	t.Log("given the need to test synchronous inserts of status updates")
+	t.Log("given the need to test the gatherer's ability to listen for status updates")
 	{
-		t.Log("\twhen update is inserted")
+		t.Log("\twhen listener runs on goroutine")
 		{
-			key := "awesomeKey"
-			value := "awesomeValue"
-			u := Update{
-				Key:   key,
-				Value: value,
-			}
-
-			g := NewGatherer()
-			g.InsertSynchronously(u)
-
-			msg := "\t\tinserted update must be present in status map"
-			if v, ok := g.status[key]; ok && v == value {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-		}
-
-		t.Log("\twhen multiple updates are performed simultaneously")
-		{
-			key := "someNumberKey"
-
 			l := &testLocker{
 				m:          sync.Mutex{},
 				numLocks:   0,
@@ -71,24 +68,70 @@ func TestGatherer_InsertSynchronously(t *testing.T) {
 				status:  map[string]interface{}{},
 				updates: make(chan Update),
 			}
-			upper := 100
-			for i := 0; i < upper; i++ {
-				go func() {
-					g.InsertSynchronously(Update{
-						Key:   key,
-						Value: rand.Intn(100),
-					})
-				}()
+
+			wg := &stateExposingWaitGroup{
+				WaitGroup: sync.WaitGroup{},
+				count:     0,
+			}
+			wg.add(1)
+			go func() {
+				defer wg.done()
+				g.Listen()
+			}()
+
+			// Listen performs initial insertion of key in question synchronously, so we can wait for the insert
+			// to be finished by watching the number of locks
+			for {
+				if l.numLocks == 1 && l.numUnlocks == 1 {
+					break
+				}
 			}
 
-			msg := "\t\tnumber of mutex locks and unlocks must be equal"
-			if l.numLocks == l.numUnlocks {
+			msg := "\t\trunner finished must be set to false in status"
+
+			if !(g.status[updateKeyRunnerFinished].(bool)) {
 				t.Log(msg, checkMark)
 			} else {
 				t.Fatal(msg, ballotX)
 			}
 
+			t.Log("\t\tupon stop signal")
+			{
+				msg = "\t\t\tlistener must set runner finished to true"
+				g.updates <- quitStatusGathering
+
+				// Wait for gatherer to finish status update
+				for {
+					if l.numLocks == 2 && l.numUnlocks == 2 {
+						break
+					}
+				}
+
+				if g.status[updateKeyRunnerFinished].(bool) {
+					t.Log(msg, checkMark)
+				} else {
+					t.Fatal(msg, ballotX)
+				}
+
+				msg = "\t\t\tlistener must close channel"
+
+				if _, channelOpen := <-g.updates; !channelOpen {
+					t.Log(msg, checkMark)
+				} else {
+					t.Fatal(msg, ballotX)
+				}
+
+				msg = "\t\t\tlistener must return, thus ending the goroutine it has been running on"
+				if wg.waitingCount() == 0 {
+					t.Log(msg, checkMark)
+				} else {
+					t.Fatal(msg, ballotX)
+				}
+
+			}
+
 		}
+
 	}
 
 }
@@ -139,6 +182,70 @@ func TestGatherer_AssembleStatusCopy(t *testing.T) {
 			} else {
 				t.Fatal(msg, ballotX)
 			}
+		}
+	}
+
+}
+
+func TestGatherer_InsertSynchronously(t *testing.T) {
+
+	t.Log("given the need to test synchronous inserts of status updates")
+	{
+		t.Log("\twhen update is inserted")
+		{
+			key := "awesomeKey"
+			value := "awesomeValue"
+			u := Update{
+				Key:   key,
+				Value: value,
+			}
+
+			g := NewGatherer()
+			g.InsertSynchronously(u)
+
+			msg := "\t\tinserted update must be present in status map"
+			if v, ok := g.status[key]; ok && v == value {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+		}
+
+		t.Log("\twhen multiple updates are performed simultaneously")
+		{
+			key := "someNumberKey"
+
+			l := &testLocker{
+				m:          sync.Mutex{},
+				numLocks:   0,
+				numUnlocks: 0,
+			}
+			g := &Gatherer{
+				l:       l,
+				status:  map[string]interface{}{},
+				updates: make(chan Update),
+			}
+			upper := 100
+			wg := sync.WaitGroup{}
+			for i := 0; i < upper; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					g.InsertSynchronously(Update{
+						Key:   key,
+						Value: rand.Intn(100),
+					})
+				}()
+			}
+			wg.Wait()
+
+			msg := "\t\tnumber of mutex locks and unlocks must be equal"
+			if l.numLocks == l.numUnlocks {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, fmt.Sprintf("num locks: %d; num unlocks: %d", l.numLocks, l.numUnlocks))
+			}
+
 		}
 	}
 
