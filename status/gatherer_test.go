ab76cb53d4754ca71708daa328916f6af705edf8
Nico Krieg <n.krieg123@googlemail.com>
2022-11-22 20:04:02 +0100

    Add test case for assembly of status copy.

--- status/gatherer_test.go
+++ status/gatherer_test.go
@@ -1,6 +1,7 @@
 package status
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
 	"testing"
@@ -89,5 +90,74 @@ func TestGatherer_InsertSynchronously(t *testing.T) {
 
 		}
 	}
+
+}
+
+func TestGatherer_GetStatusCopy(t *testing.T) {
+
+	t.Log("given the need to test retrieving a copy of the gatherer's current status")
+	{
+		t.Log("\twhen status is empty")
+		{
+			g := NewGatherer()
+
+			statusCopy := g.GetStatusCopy()
+			msg := "\t\tcopy must be empty, too"
+
+			if len(statusCopy) == 0 {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+		}
+
+		t.Log("\twhen status contains elements")
+		{
+			g := NewGatherer()
+
+			u1 := Update{"awesomeKey", "awesomeValue"}
+			g.status[u1.Key] = u1.Value
+
+			u2 := Update{"anotherKey", "anotherValue"}
+			g.status[u2.Key] = u2.Value
+
+			statusCopy := g.GetStatusCopy()
+
+			msg := "\t\tcopy and underlying status must contain same elements"
+			if equal, detail := mapsEqualInContent(g.status, statusCopy); equal {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, detail)
+			}
+
+			statusCopy[u1.Key] = "anotherAwesomeValue"
+			msg = "\t\tchange to copy must not be reflected in source map"
+
+			if g.status[u1.Key] == u1.Value {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+		}
+	}
+
+}
+
+func mapsEqualInContent(reference map[string]interface{}, candidate map[string]interface{}) (bool, string) {
+
+	if len(reference) != len(candidate) {
+		return false, "given maps do not have same length, hence cannot have equal content"
+	}
+
+	for k1, v1 := range reference {
+		if v2, ok := candidate[k1]; !ok {
+			return false, fmt.Sprintf("key wanted in candidate map, but not found: %s", k1)
+		} else if v1 != v2 {
+			return false, fmt.Sprintf("key '%s' associated with different values -- wanted: %v; got: %v", k1, v1, v2)
+		}
+	}
+
+	return true, ""
 
 }
