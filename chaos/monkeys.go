72e47c2994ff4be47ce19f4ba3dda6a31b486442
Nico Krieg <n.krieg123@googlemail.com>
2022-12-14 21:58:35 +0100

    Refactor types and functions/methods implemented thus far into common file.

--- chaos/monkeys.go
+++ chaos/monkeys.go
@@ -5,7 +5,9 @@ import (
 	log "github.com/sirupsen/logrus"
 	"hazeltest/client"
 	"hazeltest/logging"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 type (
@@ -21,6 +23,10 @@ type (
 	monkey interface {
 		init(c hzMemberChooser, k hzMemberKiller)
 		causeChaos()
+	}
+	memberKillerMonkey struct {
+		chooser hzMemberChooser
+		killer  hzMemberKiller
 	}
 	monkeyConfigBuilder struct {
 		monkeyKeyPath string
@@ -48,10 +54,78 @@ var (
 func init() {
 	lp = &logging.LogProvider{ClientID: client.ID()}
 	propertyAssigner = client.DefaultConfigPropertyAssigner{}
+	register(&memberKillerMonkey{})
 }
 
 func register(m monkey) {
 	monkeys = append(monkeys, m)
+}
+
+func (m *memberKillerMonkey) init(c hzMemberChooser, k hzMemberKiller) {
+
+	m.chooser = c
+	m.killer = k
+
+}
+
+func (m *memberKillerMonkey) causeChaos() {
+
+	// TODO Add state transitions
+	mc, err := populateMemberKillerMonkeyConfig()
+	if err != nil {
+		lp.LogInternalStateEvent("unable to populate config for member killer chaos monkey -- aborting", log.ErrorLevel)
+	}
+
+	if !mc.enabled {
+		lp.LogInternalStateEvent("member killer monkey not enabled -- won't run", log.InfoLevel)
+		return
+	}
+
+	// TODO Make API readiness dependent on chaos monkey state?
+
+	for {
+		sleep(mc.sleep)
+
+		f := rand.Float32()
+		if f <= mc.chaosProbability {
+			member, err := m.chooser.choose()
+			if err != nil {
+				lp.LogInternalStateEvent("unable to choose hazelcast member to kill -- will try again in next iteration", log.WarnLevel)
+				continue
+			}
+
+			err = m.killer.kill(member)
+			if err != nil {
+				lp.LogInternalStateEvent(fmt.Sprintf("unable to kill chosen hazelcast member '%s' -- will try again in next iteration", member.identifier), log.WarnLevel)
+			}
+		}
+	}
+
+}
+
+func sleep(sc *sleepConfig) {
+
+	if sc.enabled {
+		var sleepDuration int
+		if sc.enableRandomness {
+			sleepDuration = rand.Intn(sc.durationSeconds + 1)
+		} else {
+			sleepDuration = sc.durationSeconds
+		}
+		lp.LogInternalStateEvent(fmt.Sprintf("sleeping for '%d' seconds", sleepDuration), log.TraceLevel)
+		time.Sleep(time.Duration(sleepDuration) * time.Second)
+	}
+
+}
+
+func populateMemberKillerMonkeyConfig() (*monkeyConfig, error) {
+
+	monkeyKeyPath := "chaosMonkeys.memberKiller"
+
+	configBuilder := monkeyConfigBuilder{monkeyKeyPath: monkeyKeyPath}
+
+	return configBuilder.populateConfig()
+
 }
 
 func (b monkeyConfigBuilder) populateConfig() (*monkeyConfig, error) {
