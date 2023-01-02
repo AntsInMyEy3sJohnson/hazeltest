1680ee343273a6bfcdefc40eb232c437f033c2e5
Nico Krieg <n.krieg123@googlemail.com>
2023-01-02 22:41:49 +0100

    Introduce test interface implementations and adapt test.

--- chaos/memberaccess_test.go
+++ chaos/memberaccess_test.go
@@ -1,17 +1,49 @@
 package chaos
 
 import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"testing"
 )
 
 type (
-	testK8sConfigProvider struct{}
+	testK8sConfigBuilder        struct{}
+	testK8sClientsetInitializer struct {
+		k8sConfigBuilder
+	}
+	testK8sPodLister struct{}
 )
 
-func (t *testK8sConfigProvider) getOrInit(_ memberAccessConfig) (*rest.Config, error) {
+var (
+	testBuilder              = &testK8sConfigBuilder{}
+	testClientsetInitializer = &testK8sClientsetInitializer{testBuilder}
+	testPodLister            = &testK8sPodLister{}
+)
+
+func (b *testK8sConfigBuilder) buildForOutOfClusterAccess(_, _ string) (*rest.Config, error) {
 
 	return &rest.Config{}, nil
+
+}
+
+func (b *testK8sConfigBuilder) buildForInClusterAccess() (*rest.Config, error) {
+
+	return &rest.Config{}, nil
+
+}
+
+func (t *testK8sClientsetInitializer) getOrInit(_ memberAccessConfig) (*kubernetes.Clientset, error) {
+
+	return &kubernetes.Clientset{}, nil
+
+}
+
+func (l *testK8sPodLister) list(_ *kubernetes.Clientset, _ context.Context, _ string, _ metav1.ListOptions) (*v1.PodList, error) {
+
+	return &v1.PodList{}, nil
 
 }
 
@@ -21,11 +53,11 @@ func TestChooseMemberOnK8s(t *testing.T) {
 	{
 		t.Log("\twhen member to be killed can be identified")
 		{
-			memberChooser := k8sHzMemberChooser{configProvider: &testK8sConfigProvider{}}
+			memberChooser := k8sHzMemberChooser{testClientsetInitializer, testPodLister}
 			_, err := memberChooser.choose(assembleDummyAccessConfig())
 
 			msg := "\t\tno error must be returned"
-			if err != nil {
+			if err == nil {
 				t.Log(msg, checkMark)
 			} else {
 				t.Fatal(msg, ballotX)
