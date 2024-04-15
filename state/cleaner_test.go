e29ad81c47afbb9a7d3129a704ef0260e39408bd
AntsInMyEy3sJohnson <48876328+AntsInMyEy3sJohnson@users.noreply.github.com>
2024-04-15 21:13:56 +0200

    Add introspection on invocations, and introduce more verifications in happy-path test case.

--- state/cleaner_test.go
+++ state/cleaner_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/hazelcast/hazelcast-go-client"
+	"strings"
 	"sync"
 	"testing"
 )
@@ -30,13 +31,26 @@ type (
 	testHzClientHandler struct {
 		hzClient *hazelcast.Client
 	}
+	testHzMapStore struct {
+		maps              map[string]*testHzMap
+		getMapInvocations int
+	}
+	testHzObjectInfoStore struct {
+		objectInfos                         []simpleObjectInfo
+		getDistributedObjectInfoInvocations int
+	}
+	testHzMap struct {
+		data                map[string]any
+		evictAllInvocations int
+	}
 )
 
 const (
-	checkMark               = "\u2713"
-	ballotX                 = "\u2717"
-	mapStateCleanerBasePath = "stateCleaner.maps"
-	hzCluster               = "awesome-hz-cluster"
+	checkMark          = "\u2713"
+	ballotX            = "\u2717"
+	mapCleanerBasePath = "stateCleaner.maps"
+	hzCluster          = "awesome-hz-cluster"
+	mapCleanerName     = "awesome-map-cleaner"
 )
 
 var (
@@ -45,6 +59,37 @@ var (
 	cleanerBuildError = errors.New("something went terribly wrong when attempting to build the cleaner")
 	cleanerCleanError = errors.New("something went terribly wrong when attempting to clean state")
 )
+
+func (m *testHzMap) EvictAll(_ context.Context) error {
+
+	m.evictAllInvocations++
+
+	clear(m.data)
+
+	return nil
+
+}
+
+func (ms *testHzMapStore) GetMap(_ context.Context, name string) (hzMap, error) {
+
+	ms.getMapInvocations++
+
+	return ms.maps[name], nil
+
+}
+
+func (ois *testHzObjectInfoStore) GetDistributedObjectsInfo(_ context.Context) ([]hzObjectInfo, error) {
+
+	ois.getDistributedObjectInfoInvocations++
+
+	var hzObjectInfos []hzObjectInfo
+	for _, v := range ois.objectInfos {
+		hzObjectInfos = append(hzObjectInfos, v)
+	}
+
+	return hzObjectInfos, nil
+
+}
 
 func (ch *testHzClientHandler) InitHazelcastClient(_ context.Context, _ string, _ string, _ []string) {
 	// no-op
@@ -106,6 +151,137 @@ func (a testConfigPropertyAssigner) Assign(keyPath string, eval func(string, any
 
 }
 
+func newMapObjectInfoFromName(objectInfoName string) *simpleObjectInfo {
+	return &simpleObjectInfo{
+		name:        objectInfoName,
+		serviceName: hzMapService,
+	}
+}
+
+func newQueueObjectInfoFromName(objectInfoName string) *simpleObjectInfo {
+	return &simpleObjectInfo{
+		name:        objectInfoName,
+		serviceName: hzQueueService,
+	}
+}
+
+func populateDummyObjectsWithMapObjectInfoFromName(objectInfos *[]simpleObjectInfo, testMaps map[string]*testHzMap, nameOfNewMapObject string) {
+
+	*objectInfos = append(*objectInfos, *newMapObjectInfoFromName(nameOfNewMapObject))
+	testMaps[nameOfNewMapObject] = &testHzMap{data: make(map[string]any)}
+
+}
+
+func resolveObjectKindForNameFromObjectInfoList(name string, objectInfos []simpleObjectInfo) string {
+
+	for _, v := range objectInfos {
+		if v.getName() == name {
+			return v.getServiceName()
+		}
+	}
+
+	return ""
+
+}
+
+func TestMapCleanerClean(t *testing.T) {
+
+	t.Log("given a map cleaner build method")
+	{
+		t.Log("\twhen target hazeltest cluster contains multiple maps and queues")
+		{
+			numMapObjects := 9
+			var objectInfos []simpleObjectInfo
+			testMaps := make(map[string]*testHzMap)
+			prefixToConsider := "ht_"
+
+			for i := 0; i < numMapObjects; i++ {
+				populateDummyObjectsWithMapObjectInfoFromName(&objectInfos, testMaps, fmt.Sprintf("%sload-%d", prefixToConsider, i))
+				populateDummyObjectsWithMapObjectInfoFromName(&objectInfos, testMaps, fmt.Sprintf("gimli_load-%d", i))
+			}
+
+			// Add object representing queue, so we can verify that no attempt was made to retrieve it
+			// The name of this object matches the given predicate, so method under test must use service name to establish
+			// object in question represents queue
+			queueObjectName := fmt.Sprintf("%sload-42", prefixToConsider)
+			objectInfos = append(objectInfos, *newQueueObjectInfoFromName(queueObjectName))
+			testMaps[queueObjectName] = &testHzMap{data: make(map[string]any)}
+
+			// Add Hazelcast-internal map
+			hzInternalMapName := "__sql.catalog"
+			objectInfos = append(objectInfos, *newMapObjectInfoFromName(hzInternalMapName))
+			testMaps[hzInternalMapName] = &testHzMap{data: make(map[string]any)}
+
+			ms := &testHzMapStore{
+				maps: testMaps,
+			}
+			ois := &testHzObjectInfoStore{
+				objectInfos: objectInfos,
+			}
+			mc := mapCleaner{
+				name:      mapCleanerName,
+				hzCluster: hzCluster,
+				hzMembers: hzMembers,
+				keyPath:   mapCleanerBasePath,
+				c: &cleanerConfig{
+					enabled:   true,
+					usePrefix: true,
+					prefix:    prefixToConsider,
+				},
+				ms:  ms,
+				ois: ois,
+				ch:  &testHzClientHandler{},
+			}
+
+			ctx := context.TODO()
+			err := mc.clean(ctx)
+
+			msg := "\t\tno error must be returned"
+
+			if err == nil {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tdistributed objects info must have been queried once"
+			if ois.getDistributedObjectInfoInvocations == 1 {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tget map must have been invoked only on maps whose prefix matches configuration"
+			if ms.getMapInvocations == numMapObjects {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX, ms.getMapInvocations)
+			}
+
+			invokedOnceMsg := "\t\tevict all must have been invoked on all maps whose prefix matches configuration"
+			notInvokedMsg := "\t\tevict all must not have been invoked on data structure that is either not a map or whose name does not correspond to given prefix"
+			for k, v := range testMaps {
+				if strings.HasPrefix(k, prefixToConsider) && resolveObjectKindForNameFromObjectInfoList(k, objectInfos) == hzMapService {
+					if v.evictAllInvocations == 1 {
+						t.Log(invokedOnceMsg, checkMark, k)
+					} else {
+						t.Fatal(invokedOnceMsg, ballotX, k, v.evictAllInvocations)
+					}
+				} else {
+					if v.evictAllInvocations == 0 {
+						t.Log(notInvokedMsg, checkMark, k)
+					} else {
+						t.Fatal(notInvokedMsg, ballotX, k, v.evictAllInvocations)
+					}
+				}
+
+			}
+
+		}
+	}
+
+}
+
 func TestMapCleanerBuilderBuild(t *testing.T) {
 
 	t.Log("given a method to build a map cleaner builder")
@@ -128,7 +304,7 @@ func TestMapCleanerBuilderBuild(t *testing.T) {
 			msg = "\t\tmap cleaner built must carry map state cleaner key path"
 			mc := c.(*mapCleaner)
 
-			if mc.keyPath == mapStateCleanerBasePath {
+			if mc.keyPath == mapCleanerBasePath {
 				t.Log(msg, checkMark)
 			} else {
 				t.Fatal(msg, ballotX, mc.keyPath)
@@ -254,9 +430,9 @@ func runTestCaseAndResetState(testFunc func()) {
 func assembleTestConfig() map[string]any {
 
 	return map[string]any{
-		mapStateCleanerBasePath + ".enabled":        true,
-		mapStateCleanerBasePath + ".prefix.enabled": true,
-		mapStateCleanerBasePath + ".prefix.prefix":  "ht_",
+		mapCleanerBasePath + ".enabled":        true,
+		mapCleanerBasePath + ".prefix.enabled": true,
+		mapCleanerBasePath + ".prefix.prefix":  "ht_",
 	}
 
 }
