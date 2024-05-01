1521bed0691d00e3f1c674c1437145d4b2f86f91
AntsInMyEy3sJohnson <48876328+AntsInMyEy3sJohnson@users.noreply.github.com>
2024-05-01 17:12:47 +0100

    Introduce dedicated struct to represent status tracker, add factory function, and re-order tests.

--- api/status.go
+++ api/status.go
@@ -4,9 +4,15 @@ import (
 	"sync"
 )
 
-type ActorGroup string
-type RunnerDataStructure string
-type queryActorStateFunc func() map[string]any
+type (
+	statefulActorTracker struct {
+		sync.RWMutex
+		m map[ActorGroup]map[string]queryActorStateFunc
+	}
+	ActorGroup          string
+	RunnerDataStructure string
+	queryActorStateFunc func() map[string]any
+)
 
 const (
 	Maps   RunnerDataStructure = "maps"
@@ -21,53 +27,43 @@ const (
 )
 
 var (
-	mapRunnerStatusFunctions      sync.Map
-	queueRunnerStatusFunction     sync.Map
-	chaosMonkeyStatusFunctions    sync.Map
-	statefulActorsStatusFunctions = struct {
-		sync.RWMutex
-		m map[ActorGroup]map[string]queryActorStateFunc
-	}{m: make(map[ActorGroup]map[string]queryActorStateFunc)}
+	availableActorGroups = []ActorGroup{MapRunners, QueueRunners, ChaosMonkeys, StateCleaners}
+	tracker              = newStatefulActorTracker()
 )
+
+func newStatefulActorTracker() *statefulActorTracker {
+
+	t := &statefulActorTracker{m: make(map[ActorGroup]map[string]queryActorStateFunc)}
+
+	for _, g := range availableActorGroups {
+		t.m[g] = make(map[string]queryActorStateFunc)
+	}
+
+	return t
+
+}
 
 func RegisterStatefulActor(g ActorGroup, actorName string, queryStatusFunc func() map[string]any) {
 
-	statefulActorsStatusFunctions.Lock()
-	defer statefulActorsStatusFunctions.Unlock()
+	tracker.Lock()
+	defer tracker.Unlock()
 
-	if _, ok := statefulActorsStatusFunctions.m[g]; !ok {
-		statefulActorsStatusFunctions.m[g] = make(map[string]queryActorStateFunc)
+	if _, ok := tracker.m[g]; !ok {
+		tracker.m[g] = make(map[string]queryActorStateFunc)
 	}
 
-	statefulActorsStatusFunctions.m[g][actorName] = queryStatusFunc
-
-}
-
-func RegisterRunnerStatus(s RunnerDataStructure, source string, queryStatusFunc func() map[string]any) {
-
-	switch s {
-	case Maps:
-		mapRunnerStatusFunctions.Store(source, queryStatusFunc)
-	case Queues:
-		queueRunnerStatusFunction.Store(source, queryStatusFunc)
-	}
-
-}
-
-func RegisterChaosMonkeyStatus(source string, queryStatusFunc func() map[string]any) {
-
-	chaosMonkeyStatusFunctions.Store(source, queryStatusFunc)
+	tracker.m[g][actorName] = queryStatusFunc
 
 }
 
 func assembleActorStatus() map[ActorGroup]map[string]any {
 
-	statefulActorsStatusFunctions.RLock()
-	defer statefulActorsStatusFunctions.RUnlock()
+	tracker.RLock()
+	defer tracker.RUnlock()
 
 	result := make(map[ActorGroup]map[string]any)
 
-	for actorGroup, registeredActors := range statefulActorsStatusFunctions.m {
+	for actorGroup, registeredActors := range tracker.m {
 		result[actorGroup] = make(map[string]any)
 		for actor, actorStateQueryFunc := range registeredActors {
 			result[actorGroup][actor] = actorStateQueryFunc()
@@ -75,40 +71,5 @@ func assembleActorStatus() map[ActorGroup]map[string]any {
 	}
 
 	return result
-
-}
-
-func assembleRunnerStatus() map[ActorGroup]any {
-
-	mapTestLoopStatus := map[string]any{}
-	populateWithStatus(mapTestLoopStatus, &mapRunnerStatusFunctions)
-
-	queueTestLoopStatus := map[string]any{}
-	populateWithStatus(queueTestLoopStatus, &queueRunnerStatusFunction)
-
-	chaosMonkeyStatus := map[string]any{}
-	populateWithStatus(chaosMonkeyStatus, &chaosMonkeyStatusFunctions)
-
-	return map[ActorGroup]any{
-		MapRunners: map[RunnerDataStructure]any{
-			Maps:   mapTestLoopStatus,
-			Queues: queueTestLoopStatus,
-		},
-		ChaosMonkeys: chaosMonkeyStatus,
-	}
-
-}
-
-func populateWithStatus(target map[string]any, statusFunctionsMap *sync.Map) {
-
-	statusFunctionsMap.Range(func(key, value any) bool {
-		runnerStatus := value.(func() map[string]any)()
-		if runnerStatus != nil {
-			target[key.(string)] = runnerStatus
-		} else {
-			target[key.(string)] = map[string]any{}
-		}
-		return true
-	})
 
 }
--- api/status_test.go
+++ api/status_test.go
@@ -1,9 +1,30 @@
 package api
 
 import (
-	"sync"
 	"testing"
 )
+
+func TestNewStatefulActorTracker(t *testing.T) {
+
+	t.Log("given a factory function for the assembly of a new stateful actor tracker")
+	{
+		t.Log("\twhen assembly is invoked")
+		{
+			instance := newStatefulActorTracker()
+
+			msg := "\t\tinstance must carry map populated with empty sub-maps for each actor group"
+
+			for _, v := range availableActorGroups {
+				if subMap, ok := instance.m[v]; ok && len(subMap) == 0 {
+					t.Log(msg, checkMark)
+				} else {
+					t.Fatal(msg, ballotX)
+				}
+			}
+		}
+	}
+
+}
 
 func TestRegisterStatefulActor(t *testing.T) {
 
@@ -22,14 +43,14 @@ func TestRegisterStatefulActor(t *testing.T) {
 			RegisterStatefulActor(actorGroup, actorName, dummyFn)
 
 			msg := "\t\tlist of stateful actors must contain entry for actor group"
-			if _, ok := statefulActorsStatusFunctions.m[actorGroup]; ok {
+			if _, ok := tracker.m[actorGroup]; ok {
 				t.Log(msg, checkMark)
 			} else {
 				t.Fatal(msg, ballotX)
 			}
 
 			msg = "\t\tactor must have been stored beneath actor group"
-			actorGroupList := statefulActorsStatusFunctions.m[actorGroup]
+			actorGroupList := tracker.m[actorGroup]
 
 			if _, ok := actorGroupList[actorName]; ok {
 				t.Log(msg, checkMark)
@@ -88,17 +109,17 @@ func TestAssembleActorStatus(t *testing.T) {
 				}
 			}
 
-			msg := "\t\tthere must be one sub-map for each actor group"
+			msg := "\t\tthere must be one sub-map for each available actor group"
 
 			result := assembleActorStatus()
 
-			if len(result) == 2 {
+			if len(result) == len(availableActorGroups) {
 				t.Log(msg, checkMark)
 			} else {
 				t.Fatal(msg, ballotX, len(result))
 			}
 
-			msg = "\t\tresult must contain expected values for each actor"
+			msg = "\t\tfor each actor group with a non-zero number of registered actors, the result must contain the assembled status"
 			for actorGroup, actorsWithinGroup := range actors {
 				for actor, actorStateMap := range actorsWithinGroup {
 					for actorStateKey, actorStateValue := range actorStateMap {
@@ -111,356 +132,47 @@ func TestAssembleActorStatus(t *testing.T) {
 					}
 				}
 			}
+
+			msg = "\t\tfor each remaining actor group no actor has registered for, result must contain empty map"
+			var groupsWithoutRegisteredActor []ActorGroup
+			for _, v := range availableActorGroups {
+				if _, ok := actors[v]; !ok {
+					groupsWithoutRegisteredActor = append(groupsWithoutRegisteredActor, v)
+				}
+			}
+
+			for _, v := range groupsWithoutRegisteredActor {
+				if len(result[v]) == 0 {
+					t.Log(msg, checkMark, v)
+				} else {
+					t.Fatal(msg, ballotX, v)
+				}
+			}
+
+		}
+		t.Log("\twhen no actor has registered yet")
+		{
+			tracker = newStatefulActorTracker()
+
+			result := assembleActorStatus()
+
+			msg := "\t\tresult must contain one entry for each available actor group"
+			if len(result) == len(availableActorGroups) {
+				t.Log(msg, checkMark)
+			} else {
+				t.Fatal(msg, ballotX)
+			}
+
+			msg = "\t\tsub-map for each actor group must be empty"
+			for _, v := range availableActorGroups {
+				if len(result[v]) == 0 {
+					t.Log(msg, checkMark, v)
+				} else {
+					t.Fatal(msg, ballotX, v)
+				}
+			}
+
 		}
 	}
 
-}
-
-func TestAssembleTestLoopStatus(t *testing.T) {
-
-	t.Log("given the test loop status assembly function")
-	{
-		t.Log("\twhen no test loop has been registered")
-		{
-			resetMaps()
-
-			assembledStatus := assembleRunnerStatus()
-			msg := "\t\tstatus map must still contain two elements"
-			if len(assembledStatus) == 2 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, len(assembledStatus))
-			}
-
-			msg = "\t\tstatus map must contain one key for test loop status and chaos monkey status each"
-			if _, ok := assembledStatus[MapRunners]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, MapRunners)
-			}
-			if _, ok := assembledStatus[ChaosMonkeys]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, ChaosMonkeys)
-			}
-
-			assembledTestLoopsStatus := assembledStatus[MapRunners].(map[RunnerDataStructure]any)
-
-			msg = "\t\ttest loops map must still contain maps status"
-			if _, ok := assembledTestLoopsStatus[Maps]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\ttest loops map must still contains queues status"
-			if _, ok := assembledTestLoopsStatus[Queues]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tstatus registered for both maps and queues must be empty"
-			if len(assembledTestLoopsStatus[Maps].(map[string]any)) == 0 && len(assembledTestLoopsStatus[Queues].(map[string]any)) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tstatus for chaos monkeys must be empty"
-			chaosMonkeyStatus := assembledStatus[ChaosMonkeys].(map[string]any)
-			if len(chaosMonkeyStatus) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, len(chaosMonkeyStatus))
-			}
-
-		}
-
-		t.Log("\twhen non-empty status is provided for two map runners, two queue runners, and one chaos monkey")
-		{
-			RegisterRunnerStatus(Maps, sourceMapPokedexRunner, func() map[string]any {
-				return dummyStatusMapPokedexTestLoop
-			})
-			RegisterRunnerStatus(Maps, sourceMapLoadRunner, func() map[string]any {
-				return dummyStatusMapLoadTestLoop
-			})
-			RegisterRunnerStatus(Queues, sourceQueueTweetRunner, func() map[string]any {
-				return dummyStatusQueueTweetTestLoop
-			})
-			RegisterRunnerStatus(Queues, sourceQueueLoadRunner, func() map[string]any {
-				return dummyStatusQueueLoadTestLoop
-			})
-			RegisterChaosMonkeyStatus(sourceChaosMonkeyMemberKiller, func() map[string]any {
-				return dummyStatusMemberKillerMonkey
-			})
-
-			assembledStatus := assembleRunnerStatus()
-			msg := "\t\tassembled map must contain two top-level keys"
-			if len(assembledStatus) == 2 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, len(assembledStatus))
-			}
-
-			msg = "\t\tassembled map must contain one top-level key for test loop status and chaos monkey status each"
-			if _, ok := assembledStatus[MapRunners]; ok {
-				t.Log(msg, checkMark, MapRunners)
-			} else {
-				t.Fatal(msg, ballotX, MapRunners)
-			}
-			if _, ok := assembledStatus[ChaosMonkeys]; ok {
-				t.Log(msg, checkMark, ChaosMonkeys)
-			} else {
-				t.Fatal(msg, ballotX, ChaosMonkeys)
-			}
-
-			assembledTestLoopsStatus := assembledStatus[MapRunners].(map[RunnerDataStructure]any)
-
-			msg = "\t\ttest loops map must contain maps status"
-			if _, ok := assembledTestLoopsStatus[Maps]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-			msg = "\t\ttest loops map must contain queues status"
-			if _, ok := assembledTestLoopsStatus[Queues]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tmaps map must contain keys for both registered map-type sources"
-			assembledStatusMaps := assembledTestLoopsStatus[Maps].(map[string]any)
-			if _, ok := assembledStatusMaps[sourceMapPokedexRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapPokedexRunner)
-			}
-			if _, ok := assembledStatusMaps[sourceMapLoadRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapLoadRunner)
-			}
-
-			msg = "\t\tqueues map must contain keys for both registered queue-type source"
-			assembledStatusQueues := assembledTestLoopsStatus[Queues].(map[string]any)
-			if _, ok := assembledStatusQueues[sourceQueueTweetRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueTweetRunner)
-			}
-			if _, ok := assembledStatusQueues[sourceQueueLoadRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueLoadRunner)
-			}
-
-			msg = "\t\tvalues contained in assembled status for maps must mirror provided test loop status"
-			assembledStatusMapPokedexTestLoop := assembledStatusMaps[sourceMapPokedexRunner].(map[string]any)
-			if ok, detail := mapsEqualInContent(dummyStatusMapPokedexTestLoop, assembledStatusMapPokedexTestLoop); ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, detail)
-			}
-			msg = "\t\tvalues contained in assembled status must mirror provided test loop status"
-			assembledStatusMapLoadTestLoop := assembledStatusMaps[sourceMapLoadRunner].(map[string]any)
-			if ok, detail := mapsEqualInContent(dummyStatusMapLoadTestLoop, assembledStatusMapLoadTestLoop); ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, detail)
-			}
-
-			msg = "\t\tvalues contained in assembled status for queues must mirror provided test loop status"
-			assembledStatusQueueTweetTestLoop := assembledStatusQueues[sourceQueueTweetRunner].(map[string]any)
-			if ok, detail := mapsEqualInContent(dummyStatusQueueTweetTestLoop, assembledStatusQueueTweetTestLoop); ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, detail)
-			}
-			assembledStatusQueueLoadTestLoop := assembledStatusQueues[sourceQueueLoadRunner].(map[string]any)
-			if ok, detail := mapsEqualInContent(dummyStatusQueueLoadTestLoop, assembledStatusQueueLoadTestLoop); ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, detail)
-			}
-
-			msg = "\t\tchaos monkey status must contain exactly one key"
-			chaosMonkeyStatus := assembledStatus[ChaosMonkeys].(map[string]any)
-			if len(chaosMonkeyStatus) == 1 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tassembled member killer status must mirror given test status"
-			memberKillerStatus := chaosMonkeyStatus[sourceChaosMonkeyMemberKiller].(map[string]any)
-			if ok, detail := mapsEqualInContent(dummyStatusMemberKillerMonkey, memberKillerStatus); ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, detail)
-			}
-
-		}
-
-		t.Log("\twhen function for querying status yields empty map")
-		{
-			resetMaps()
-
-			RegisterRunnerStatus(Maps, sourceMapPokedexRunner, func() map[string]any {
-				return map[string]any{}
-			})
-			RegisterRunnerStatus(Queues, sourceQueueTweetRunner, func() map[string]any {
-				return map[string]any{}
-			})
-			RegisterChaosMonkeyStatus(sourceChaosMonkeyMemberKiller, func() map[string]any {
-				return map[string]any{}
-			})
-
-			assembledStatus := assembleRunnerStatus()
-			assembledTestLoopsStatus := assembledStatus[MapRunners].(map[RunnerDataStructure]any)
-
-			msg := "\t\ttest loops map must contain key for maps"
-			if _, ok := assembledTestLoopsStatus[Maps]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\ttest loops map must contain key for queues"
-			if _, ok := assembledTestLoopsStatus[Queues]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tmaps status must contain key for registered map test loop"
-			assembledStatusMaps := assembledTestLoopsStatus[Maps].(map[string]any)
-			if _, ok := assembledStatusMaps[sourceMapPokedexRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapPokedexRunner)
-			}
-
-			assembledStatusQueues := assembledTestLoopsStatus[Queues].(map[string]any)
-			if _, ok := assembledStatusQueues[sourceQueueTweetRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueTweetRunner)
-			}
-
-			msg = "\t\tstatus map must be empty"
-			if len(assembledStatusMaps[sourceMapPokedexRunner].(map[string]any)) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapPokedexRunner)
-			}
-
-			if len(assembledStatusQueues[sourceQueueTweetRunner].(map[string]any)) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueTweetRunner)
-			}
-
-			msg = "\t\tchaos monkey status must contain exactly one key"
-			assembledChaosMonkeyStatus := assembledStatus[ChaosMonkeys].(map[string]any)
-			if len(assembledChaosMonkeyStatus) == 1 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, len(assembledChaosMonkeyStatus))
-			}
-
-			msg = "\t\tmember killer status must be empty"
-			memberKillerStatus := assembledChaosMonkeyStatus[sourceChaosMonkeyMemberKiller].(map[string]any)
-			if len(memberKillerStatus) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, len(memberKillerStatus))
-			}
-		}
-
-		t.Log("\twhen function for querying status yields nil")
-		{
-			resetMaps()
-
-			RegisterRunnerStatus(Maps, sourceMapLoadRunner, func() map[string]any {
-				return nil
-			})
-			RegisterRunnerStatus(Queues, sourceQueueTweetRunner, func() map[string]any {
-				return nil
-			})
-			RegisterChaosMonkeyStatus(sourceChaosMonkeyMemberKiller, func() map[string]any {
-				return nil
-			})
-
-			assembledStatus := assembleRunnerStatus()
-			assembledTestLoopsStatus := assembledStatus[MapRunners].(map[RunnerDataStructure]any)
-
-			msg := "\t\ttest loops map must still contain top-level key for map status"
-			if _, ok := assembledTestLoopsStatus[Maps]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\ttest loops map must still contain top-level key for queue status"
-			if _, ok := assembledTestLoopsStatus[Queues]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tmaps status must contain key for registered map test loop"
-			assembledStatusMaps := assembledTestLoopsStatus[Maps].(map[string]any)
-			if _, ok := assembledStatusMaps[sourceMapLoadRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapLoadRunner)
-			}
-
-			assembledStatusQueues := assembledTestLoopsStatus[Queues].(map[string]any)
-			if _, ok := assembledStatusQueues[sourceQueueTweetRunner]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueTweetRunner)
-			}
-
-			msg = "\t\tstatus map must be empty"
-			if len(assembledStatusMaps[sourceMapLoadRunner].(map[string]any)) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceMapLoadRunner)
-			}
-
-			if len(assembledStatusQueues[sourceQueueTweetRunner].(map[string]any)) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX, sourceQueueTweetRunner)
-			}
-
-			assembledChaosMonkeyStatus := assembledStatus[ChaosMonkeys].(map[string]any)
-			msg = "\t\tchaos monkey status must still contain top-level key for member killer monkey"
-			if _, ok := assembledChaosMonkeyStatus[sourceChaosMonkeyMemberKiller]; ok {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-
-			msg = "\t\tmember killer status must be empty"
-			memberKillerStatus := assembledChaosMonkeyStatus[sourceChaosMonkeyMemberKiller].(map[string]any)
-			if len(memberKillerStatus) == 0 {
-				t.Log(msg, checkMark)
-			} else {
-				t.Fatal(msg, ballotX)
-			}
-		}
-
-	}
-
-}
-
-func resetMaps() {
-	mapRunnerStatusFunctions = sync.Map{}
-	queueRunnerStatusFunction = sync.Map{}
-	chaosMonkeyStatusFunctions = sync.Map{}
 }
