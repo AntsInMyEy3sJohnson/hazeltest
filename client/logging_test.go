9677700d467b5bd13ecfd3e74f1ca28455e80421
AntsInMyEy3sJohnson <48876328+AntsInMyEy3sJohnson@users.noreply.github.com>
2026-07-10 16:05:43 +0200

    Provide first iteration of test to verify assembled log-event-to-log-level config for each logging component.

--- client/logging_test.go
+++ client/logging_test.go
@@ -2,7 +2,10 @@ package client
 
 import (
 	"errors"
+	"fmt"
 	"testing"
+
+	"go.uber.org/zap/zapcore"
 )
 
 func TestInitLoggingComponents(t *testing.T) {
@@ -49,32 +52,36 @@ func TestInitLoggingComponents(t *testing.T) {
 			}
 		}
 
-		t.Log("\t\tgiven a populated logging config")
+		t.Log(oneTab + "given a populated logging config")
 		{
-			t.Log("\t\t\twhen logging config content is valid")
+			t.Log(twoTabs + "when logging config content is valid")
 			{
+				mapRunnerComponent := "mapRunner"
+				mapRunnerLoggingEvents := map[string]any{
+					"runnerEvent":    "INFO",
+					"ioEvent":        "WARN",
+					"hazelcastEvent": "WARN",
+					"timingEvent":    "DEBUG",
+				}
+				queueRunnerComponent := "queueRunner"
+				queueRunnerLoggingEvents := map[string]any{
+					"runnerEvent": "WARN",
+					"timingEvent": "DEBUG",
+				}
 				loggingConfig = map[string]any{
 					"logging": map[string]any{
 						"level": map[string]any{
 							"root": "INFO",
 							"components": map[string]any{
-								"mapRunner": map[string]any{
-									"runnerEvent":    "INFO",
-									"ioEvent":        "WARN",
-									"hazelcastEvent": "WARN",
-									"timingEvent":    "DEBUG",
-								},
-								"queueRunner": map[string]any{
-									"runnerEvent": "WARN",
-									"timingEvent": "DEBUG",
-								},
+								mapRunnerComponent:   mapRunnerLoggingEvents,
+								queueRunnerComponent: queueRunnerLoggingEvents,
 							},
 						},
 					},
 				}
 				err := InitLoggingComponents()
 
-				msg := "\t\t\t\tno error must be returned"
+				msg := threeTabs + "no error must be returned"
 
 				if err == nil {
 					t.Log(msg, checkMark)
@@ -82,16 +89,70 @@ func TestInitLoggingComponents(t *testing.T) {
 					t.Fatal(msg, ballotX)
 				}
 
-				msg = "\t\t\t\tlog levels must have been initialized"
+				msg = threeTabs + "log levels must have been initialized"
 
 				// Given logging config contains event-to-log-level configuration for
 				// two components
-				if len(logLevels.componentsConfig) == 2 {
+				componentsConfig := logLevels.componentsConfig
+				if len(componentsConfig) == 2 {
 					t.Log(msg, checkMark)
 				} else {
 					t.Fatal(msg, ballotX)
 				}
 
+				expectedLoggingComponentsConfig := map[string]map[string]any{
+					mapRunnerComponent:   mapRunnerLoggingEvents,
+					queueRunnerComponent: queueRunnerLoggingEvents,
+				}
+
+				msgNumElements := threeTabs + "assembled logging component config must contain expected number of elements"
+				msgEventAndLevelMatch := threeTabs + "assembled logging component config must contain expected log-event-to-level mappings"
+				for component, expectedConfig := range expectedLoggingComponentsConfig {
+					assembledConfig := componentsConfig[component]
+
+					if len(expectedConfig) == len(assembledConfig.eventLevels) {
+						t.Log(msgNumElements, checkMark, component)
+					} else {
+						t.Fatal(msgNumElements, ballotX, component)
+					}
+
+					eventLevels := assembledConfig.eventLevels
+
+					for expectedEvent, expectedLevel := range expectedConfig {
+
+						if assembledLevel, ok := eventLevels[logEvent(expectedEvent)]; !ok {
+							t.Fatal(msgEventAndLevelMatch, ballotX, component, expectedEvent)
+						} else {
+							expectedLevelString := expectedLevel.(string)
+							var levelMatches func(actual zapcore.Level) bool
+							switch expectedLevelString {
+							case "DEBUG":
+								levelMatches = func(actual zapcore.Level) bool {
+									return actual == zapcore.DebugLevel
+								}
+							case "INFO":
+								levelMatches = func(actual zapcore.Level) bool {
+									return actual == zapcore.InfoLevel
+								}
+							case "WARN":
+								levelMatches = func(actual zapcore.Level) bool {
+									return actual == zapcore.WarnLevel
+								}
+							case "ERROR":
+								levelMatches = func(actual zapcore.Level) bool {
+									return actual == zapcore.ErrorLevel
+								}
+							default:
+								t.Fatal(msg, ballotX, fmt.Sprintf("erroneous test setup; no support for expected logging level '%s'", expectedLevelString))
+							}
+							if levelMatches(assembledLevel) {
+								t.Log(msg, checkMark, component, expectedEvent, assembledLevel)
+							} else {
+								t.Fatal(msg, ballotX, component, expectedEvent, assembledLevel)
+							}
+						}
+					}
+				}
 			}
 		}
 
